a76687977fde9344a73763ccc11ca87d6628a65e
Chesvic Esuani <lordgape@gmail.com>
2019-03-29 14:16:59 +0100

    Seed database table

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+
+        'name' => $faker->word,
+        'details' => $faker->paragraph,
+        'price' => $faker->randomFloat(2,100,10000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2,30),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Product;
+use App\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory(Product::class,50)->create();
+        factory(Review::class,300)->create();
     }
 }
